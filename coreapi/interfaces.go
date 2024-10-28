3619fcdcbed8887cf9313a8689cf91e8b4e87a0a
SyaibanAhmadRamadhan <ibanrama29@gmail.com>
2024-10-28 21:35:05 +0700

    added ewallet sdk

--- coreapi/interfaces.go
+++ coreapi/interfaces.go
@@ -1,7 +1,21 @@
-package coreapi
+package coreapi_midtrans
 
 import "context"
 
+type PaymentAPI interface {
+	ChargeEWalletAPI
+}
+
 type ChargeEWalletAPI interface {
+	// ChargeQRIS
+	// list error type: ErrMarshaller, ErrApiCall, ErrDuplicateOrderID, ErrRateLimitExceeded
 	ChargeQRIS(ctx context.Context, input ChargeQRISInput) (output ChargeQRISOutput, err error)
+
+	// ChargeGoPay
+	// list error type: ErrMarshaller, ErrApiCall, ErrDuplicateOrderID, ErrRateLimitExceeded
+	ChargeGoPay(ctx context.Context, input ChargeGoPayInput) (output ChargeGoPayOutput, err error)
+
+	// ChargeShopeePay
+	// list error type: ErrMarshaller, ErrApiCall, ErrDuplicateOrderID, ErrRateLimitExceeded
+	ChargeShopeePay(ctx context.Context, input ChargeShopeePayInput) (output ChargeShopeePayOutput, err error)
 }
